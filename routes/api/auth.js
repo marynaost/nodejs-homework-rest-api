a8d02812778d5804cf9cd280514aea314d7bdd58
Maryna Ostapliuk <81308158+marynaost@users.noreply.github.com>
2022-04-29 15:09:40 +0300

    added pagination

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const validation = require("../../middlewares/validations");
+const auth = require("../../middlewares/auth");
 const ctrlWrapper = require("../../middlewares/ctrlWrapper");
 const { signup, login, logout } = require("../../controllers/auth");
 const { joiSignupSchema, joiLoginSchema } = require("../../models/user");
@@ -7,6 +8,6 @@ const router = express.Router();
 
 router.post("/signup", validation(joiSignupSchema), ctrlWrapper(signup));
 router.post("/login", validation(joiLoginSchema), ctrlWrapper(login));
-router.post("/logout", ctrlWrapper(logout));
+router.post("/logout", auth, ctrlWrapper(logout));
 
 module.exports = router;
